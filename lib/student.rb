f6109db719fd09647bb2626156329f87ed1ec808
Kristen Clunie <kcluniesa@yahoo.com>
2019-02-20 02:12:44 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -28,7 +28,14 @@ class Student
   end
   
   def save
-    
+        sql = <<-SQL
+      INSERT INTO songs (name, album) 
+      VALUES (?, ?)
+    SQL
+ 
+    DB[:conn].execute(sql, self.name, self.album)
+ 
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
   end
   
 end
